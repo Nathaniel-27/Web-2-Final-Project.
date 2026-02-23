6d8c544214908354e524b81871f299715f8f4327
Nathaniel Johns <johnsn@students.westerntc.edu>
2026-02-23 14:39:44 -0600

    Lets go more CSS is getting completed!!!

--- app.js
+++ app.js
@@ -56,27 +56,7 @@ app.get('/contact', (req, res) => {
   });
 });
 
-/* app.post('/contact/submit', (req, res) => {
 
-  // Extract the form input from the request body:
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
-  const comments = req.body.comments;
-  // Note: You could extract the form input with a single line that 'de-structures' the request body:
-  //const {firstName, lastName, email, comments} = req.body;
-
-  // Make sure none of the variables are empty (falsy)
-  if(firstName && lastName && email && comments){
-    res.send("TODO: more validation and send an email");
-    // TODO: make sure the email entered into the form is a valid email address
-    // TODO: make sure the form does not include spam
-    // TODO: send an email to YOUR email address with the data entered into the form
-  }else{
-    res.status(400).send("Invalid request - there is at least one empty input in the form")
-  }
-
-}); */
 app.post('/contact/submit', (req, res) => {
 
   // import the contact-helper functions that we need
